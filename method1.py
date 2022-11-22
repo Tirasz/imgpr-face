ce4c3691103a05da99f95644cf05f6021e8aa294
Tirasz <zacsi143@gmail.com>
2022-11-22 15:52:13 +0100

    masking kinda works

--- method1.py
+++ method1.py
@@ -4,41 +4,7 @@ from matplotlib import pyplot as plt
 from pathlib import Path
 from itertools import chain
 
-def morph(img, size = 7, shape = cv2.MORPH_RECT, dil_iters = 1, er_iters = 2):
-    struct = cv2.getStructuringElement(shape, (size, size))
-    dilated = cv2.dilate(img, struct, iterations=dil_iters)
-    eroded = cv2.erode(dilated, struct, iterations=er_iters)
-    return cv2.dilate(eroded, struct)
-
-def preprocess(img, filename='', testMode = False):
-    # nomralize +median blur + morphing for noise
-    tr_img = np.zeros((img.shape))
-    tr_img = cv2.normalize(img, tr_img, 0,255, cv2.NORM_MINMAX)
-    tr_img = morph(cv2.medianBlur(tr_img, 7), dil_iters=2, er_iters=1, size=3)
-    return tr_img
 
 
 def detectAndDisplay(img):
-    img = preprocess(img)
-    rows, cols, channels = img.shape
-    # RGB Szinmodell 2D normalizált színtérbe (+1 h ne baszodjon el)
-    B,G,R = np.array(cv2.split(img), dtype=np.float32)
-    r = R / (R+G+B+1)
-    g = G / (R+G+B+1)
-
-    # Normalizált csatornák oszlopvektorokká alakítása cv.kmeans miatt
-    r_flat = r.reshape((-1, 1))
-    g_flat = g.reshape((-1, 1))
-    b_zeros = np.float32(np.zeros((rows, cols)).reshape((-1,1)))
-
-    # KMEANS setup, futtatás
-    criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, 20, 1.0) # Megállási feltétel(ek): max iterációk: 10, minimum pontosság: 1
-    K = 6 # HÁNY KLASZTER
-    k_means_data = np.column_stack((b_zeros, r_flat, g_flat)) # Kmeans adata: a jellemzok oszlopok
-    ret,label,center=cv2.kmeans(k_means_data,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
-
-    # Eredmények
-    res = center[label.flatten()]
-    res2 = res.reshape((img.shape))
-    res3 = np.uint8(res2 * 255)
-    return res3
+    return img
--- method2.py
+++ method2.py
@@ -1,5 +1,91 @@
 import cv2
+import numpy as np
+from scipy.signal import medfilt2d
+
+def L(M):
+    # L(x) = 105*logbaseten(x+1+n)
+    # The constant 105 simply scales the output of the log function into the range [0,254].
+    # n is a random noise value, generated from a distribution uniform over the range [0,1).
+    # The random noise is added to prevent banding artifacts in dark areas of the image.
+    # The constant 1 added before the log transformation prevents excessive inflation of color distinctions in very dark regions.
+    return (105 * np.log10(M + np.random.random(M.shape)+ 1))
+
+def getKernel(size):
+    size = int(size)
+    if size%2:
+        return size
+    return size + 1
 
 def detectAndDisplay(img):
     rows, cols, channels = img.shape
-    return cv2.rectangle(img, (0, 0), (rows, cols), (0,255,0), 2)
+
+    # https://www.cs.hmc.edu/~fleck/naked-skin.html
+    # The zero-response of the camera is estimated as the smallest value in any of the three color planes,
+    #  omitting locations within 10 pixels of the image edges.
+    #  This value is subtracted from each R, G, and B value.
+    #  This avoids potentially significant desaturation of opponent color values if the zero-response is far from zero.
+
+    with_border = cv2.rectangle(img, (0,0), (cols, rows), (255,255,255), 10)
+    zero_response = np.amin(with_border)
+
+    B, G, R = cv2.split(img)
+    R = R - zero_response
+    G = G - zero_response
+    B = B - zero_response
+
+    # The RGB values are then transformed into log-opponent values I, Rg, and By as follows:
+        #L(x) = 105*logbaseten(x+1+n)
+        #I = L(G)
+        #Rg = L(R) - L(G)
+        #By = L(B) - (L(G) + L(R))/2
+    # The green channel is used to represent intensity because the red and blue channels from some cameras have poor spatial resolution.
+    # The log transformation makes the Rg and By values, as well as differences between I values (e.g. texture amplitude), independent of illumination level.
+    # return 105 * math.log10(x+1+n)
+    # I = np.float64(CALC_L(G))
+
+    LG = np.float32(L(G))
+    LR = np.float32(L(R))
+    I = np.float32(LG) 
+    Rg = LR - LG 
+    By = L(B) - (LG + LR)/2
+
+    # The algorithm computes a scaling factor SCALE, which measures the size of the image.
+    # SCALE is the sum of the height and the width of the image (in pixels), divided by 320.
+    # Spatial constants used in the skin filter are functions of SCALE, so that their output does not depend on the resolution of the input image.
+    SCALE = (rows + cols) / 320
+
+    # The Rg and By arrays are smoothed with a median filter of radius 2*SCALE, to reduce noise.
+
+    # Rg = medfilt2d(Rg, getKernel(SCALE*2))
+    # By = medfilt2d(By, getKernel(SCALE*2))
+    Rg = medfilt2d(Rg, getKernel(SCALE*2))
+    By = medfilt2d(By, getKernel(SCALE*2))
+
+
+    # To compute texture amplitude, the intensity image was smoothed with a median filter of radius 4*SCALE. 
+    # The result was subtracted from the original image.
+    # The absolute values of these differences are then run through a second median filter of radius 6*SCALE. 
+    SMOOTH_I = medfilt2d(I, getKernel(SCALE*4))
+    DIFF = np.abs(I - SMOOTH_I)
+    TEXTURE = medfilt2d(DIFF, getKernel(SCALE*6))
+
+
+    # For convenience, define the hue at a pixel to be atan(Rg,By), where Rg and By are the smoothed values computed as in the previous section.
+    HUE = np.degrees(np.arctan2(Rg, By))
+
+    # The saturation at the pixel is sqrt(Rg^2 + By^2). 
+    SAT = np.sqrt(np.square(Rg) + np.square(By))
+
+    # The first (tightly-tuned) stage of the skin filter marks all pixels whose texture amplitude is no larger than 5,
+    # and (a) whose hue is between 110 and 150 and whose saturation is between 20 and 60 
+    # or (b) whose hue is between 130 and 170 and whose saturation is between 30 and 130.
+    mask = np.zeros((rows, cols), dtype=np.uint8)
+    
+    mask[(TEXTURE < 5) & (HUE > 110) & (HUE < 150) & (SAT > 20) & (SAT < 60)] = 255
+    mask[(TEXTURE < 5) & (HUE > 130) & (HUE < 170) & (SAT > 30) & (SAT < 130)] = 255
+    # The skin regions are cleaned up and enlarged slightly, to accomodate possible desaturated regions adjacent to the marked regions.
+    #  Specifically, a pixel is marked in the final map if at least one eighth of the pixels in a circular neighborhood of radius
+    #  24*SCALE pixels are marked in the original map. This is done quickly (though only approximately) by slightly modifying the fast median filter.
+    mask = cv2.dilate(mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(SCALE*12), int(SCALE*12))))
+
+    return cv2.bitwise_and(img,img,mask = mask)
